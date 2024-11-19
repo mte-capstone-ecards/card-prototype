e90e4398045fcb93da94740478cb53a651c96458
GuyBlumenthal <guy.blumenthal@outlook.com>
2024-11-19 18:32:55 -0500

    Initial commit!

---
+++ App/Src/app.c
@@ -0,0 +1,23 @@
+
+#include "app.h"
+#include "main.h"
+
+void app_main(void)
+{
+
+	while (1)
+	{
+#if defined(BOARD_NUCLEO)
+
+#elif defined(BOARD_CARD)
+
+#endif
+
+#if defined(BOARD_G4)
+
+#endif
+		HAL_GPIO_TogglePin(USER_LED_GPIO_Port, USER_LED_Pin);
+		HAL_Delay(500);
+	}
+
+}
