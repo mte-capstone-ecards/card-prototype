2ebaa5fd0f64691b5a3a2f18c7e7be362d05a368
GuyBlumenthal <guy.blumenthal@outlook.com>
2024-12-16 11:11:54 -0500

    Added initial L071 card board revision

--- App/Src/app.c
+++ App/Src/app.c
@@ -8,23 +8,10 @@ void app_main(void)
 	while (1)
 	{
 #if defined(BOARD_NUCLEO)
-
-#elif defined(BOARD_CARD)
-
-#endif
-
-#if defined(BOARD_F4)
-
-#elif defined(BOARD_G4)
-
-#endif
-
-#if defined(BOARD_G4)
-
-#endif
 		HAL_GPIO_TogglePin(USER_LED_GPIO_Port, USER_LED_Pin);
 
 		HAL_Delay(1000);
+#endif
 	}
 
 }
---
+++ Board/SVD/README.md
@@ -0,0 +1,3 @@
+### Finding SVD Files
+
+For ST chips, go to [this link](https://www.st.com/en/microcontrollers-microprocessors/stm32-32-bit-arm-cortex-mcus.html), select your product line and go under CAD Resources > System View Description
